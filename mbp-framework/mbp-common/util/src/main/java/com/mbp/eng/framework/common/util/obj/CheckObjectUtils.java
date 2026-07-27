9e87c46bd61a1274e9093357257300e87569448e
cpsxpl <cpplsx@163.com>
2026-07-27 22:14:44 +0800

    update

--- mbp-framework/mbp-common/util/src/main/java/com/mbp/eng/framework/common/util/obj/CheckObjectUtils.java
+++ mbp-framework/mbp-common/util/src/main/java/com/mbp/eng/framework/common/util/obj/CheckObjectUtils.java
@@ -1,14 +1,55 @@
 package com.mbp.eng.framework.common.util.obj;
 
+import cn.hutool.core.util.ObjUtil;
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.StrUtil;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.mbp.eng.framework.common.util.date.DateUtil;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CheckObjectUtils {
     private static final Logger logger = LoggerFactory.getLogger(CheckObjectUtils.class);
+
+    /***********************↓↓↓↓↓↓单例(双重校验锁)↓↓↓↓↓↓↓*****************/
+    private static volatile CheckObjectUtils instance;
+
+    private CheckObjectUtils() {
+        long time = System.currentTimeMillis();
+        logger.info("==========单例(双重校验锁)_time:{}", DateUtil.getFormatTime(time));
+    }
+
+    public static CheckObjectUtils getInstance() {
+        if (instance == null) {
+            synchronized (CheckObjectUtils.class) {
+                if (instance == null)
+                    //instance为 volatile,现在没问题了
+                    instance = new CheckObjectUtils();
+            }
+        }
+        return instance;
+    }
+
+    /***********************↑↑↑↑↑↑单例(双重校验锁)↑↑↑↑↑↑↑*****************/
+
+    private static final Map<Class<?>, Map<String, Field>> FIELD_CACHE = new ConcurrentHashMap<>();
 
     /**
      * 判断Object对象不为空且为数字
@@ -41,6 +82,7 @@ public class CheckObjectUtils {
         }
         return flag;
     }
+
     /**
      * 判断对象是否为空,且对象的所有属性都为空
      * boolean类型会有默认值false 判断结果不会为null 会影响判断结果
@@ -72,5 +114,87 @@ public class CheckObjectUtils {
             }
         }
         return flag;
+    }
+
+
+    public <T> void getCurrentName(T t) {
+        Class<?> clz = t.getClass();
+        long time = System.currentTimeMillis();
+        String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
+        logger.info("=====类:{}**********方法:{} time: is {}", clz, methodName, DateUtil.getFormatTime(time));
+    }
+
+
+    public void serialize(LocalDateTime value, JsonGenerator gen) {
+        String fieldName = gen.getOutputContext().getCurrentName();
+        if (fieldName != null) {
+            Object currentValue = gen.getOutputContext().getCurrentValue();
+            if (currentValue != null) {
+                Class<?> clazz = currentValue.getClass();
+                Map<String, Field> fieldMap = FIELD_CACHE.computeIfAbsent(clazz, this::buildFieldMap);
+                Field field = fieldMap.get(fieldName);
+                if (field != null && field.isAnnotationPresent(JsonFormat.class)) {
+                    JsonFormat jsonFormat = field.getAnnotation(JsonFormat.class);
+                    try {
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(jsonFormat.pattern());
+                        gen.writeString(formatter.format(value));
+                        return;
+                    } catch (Exception ex) {
+                        logger.warn("[serialize][({}#{}) 使用 JsonFormat pattern 失败,尝试使用默认的 Long 时间戳]",
+                                clazz.getName(), fieldName, ex);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 构建字段映射（缓存）
+     *
+     * @param clazz 类
+     * @return 字段映射
+     */
+    private Map<String, Field> buildFieldMap(Class<?> clazz) {
+        Map<String, Field> fieldMap = new HashMap<>();
+        for (Field field : ReflectUtil.getFields(clazz)) {
+            String fieldName = field.getName();
+            JsonProperty jsonProperty = field.getAnnotation(JsonProperty.class);
+            if (jsonProperty != null) {
+                String value = jsonProperty.value();
+                if (StrUtil.isNotEmpty(value) && ObjUtil.notEqual("\u0000", value)) {
+                    fieldName = value;
+                }
+            }
+            fieldMap.put(fieldName, field);
+        }
+        return fieldMap;
+    }
+
+    public void test() {
+        // 方法1：通过System.getProperty获取类路径
+        String classpath = System.getProperty("java.class.path");
+        System.out.println("方法1 - 系统类路径: " + classpath);
+
+        // 方法2：通过当前类的ClassLoader获取资源路径
+        URL resource = CheckObjectUtils.class.getResource("");
+        System.out.println("方法2 - 当前类所在路径: " + resource.getPath());
+
+        // 方法3：通过ClassLoader获取根路径
+        URL rootResource = CheckObjectUtils.class.getClassLoader().getResource("");
+        if (rootResource != null) {
+            System.out.println("方法3 - 类加载器根路径: " + rootResource.getPath());
+        }
+
+        // 方法4：获取当前工作目录
+        String workingDir = System.getProperty("user.dir");
+        System.out.println("方法4 - 当前工作目录: " + workingDir);
+
+        // 方法5：使用File获取规范路径
+        try {
+            File file = new File(".");
+            System.out.println("方法5 - 当前目录规范路径: " + file.getCanonicalPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
